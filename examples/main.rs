1df529d930adecc14ce5be98d9766d5a692ac9ba
Michiel <michielp1807@gmail.com>
2024-12-13 16:16:39 +0100

    Update example usage command

--- examples/main.rs
+++ examples/main.rs
@@ -6,7 +6,7 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
         println!("Example usage:");
-        println!("$ sudo ./target/debug/pps-time /dev/pps0");
+        println!("$ sudo ./target/debug/examples/main /dev/pps0");
         return;
     }
 
