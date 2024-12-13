1622cb46943bd7a333910a301a7a8ccb0143b934
Michiel <michielp1807@gmail.com>
2024-12-13 15:45:39 +0100

    Clean up error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::File,
-    io::Error,
+    io::{Error, Result},
     mem::MaybeUninit,
     os::{
         fd::AsRawFd,
@@ -21,39 +21,38 @@ const PPS_FETCH: c_ulong = 0xc00870a4;
 pub struct PpsDevice(File);
 
 impl PpsDevice {
-    pub fn new(path: PathBuf) -> Result<PpsDevice, Error> {
+    pub fn new(path: PathBuf) -> Result<PpsDevice> {
         Ok(PpsDevice(File::open(path)?))
     }
 
     /// Perform ioctl request and check result for possible errors
-    unsafe fn ioctl<T>(&self, request: c_ulong, value: &mut T) -> Result<(), Error> {
-        let result = libc::ioctl(self.0.as_raw_fd(), request, value);
-        if result != 0 {
-            return Err(Error::last_os_error());
+    unsafe fn ioctl<T>(&self, request: c_ulong, value: &mut T) -> Result<()> {
+        match libc::ioctl(self.0.as_raw_fd(), request, value) {
+            0 => Ok(()),
+            _ => Err(Error::last_os_error()),
         }
-        Ok(())
     }
 
     /// Perform ioctl request with uninitialized data
-    unsafe fn ioctl_uninit<T>(&self, request: c_ulong) -> Result<T, Error> {
+    unsafe fn ioctl_uninit<T>(&self, request: c_ulong) -> Result<T> {
         let mut value: MaybeUninit<T> = MaybeUninit::uninit();
         self.ioctl(request, &mut value)?;
         Ok(unsafe { value.assume_init() })
     }
 
-    pub fn get_params(&self) -> Result<pps_kparams, Error> {
+    pub fn get_params(&self) -> Result<pps_kparams> {
         unsafe { self.ioctl_uninit(PPS_GETPARAMS) }
     }
 
-    pub fn set_params(&self, params: &mut pps_kparams) -> Result<(), Error> {
+    pub fn set_params(&self, params: &mut pps_kparams) -> Result<()> {
         unsafe { self.ioctl(PPS_SETPARAMS, params) }
     }
 
-    pub fn get_cap(&self) -> Result<c_uint, Error> {
+    pub fn get_cap(&self) -> Result<c_uint> {
         unsafe { self.ioctl_uninit(PPS_GETCAP) }
     }
 
-    pub fn fetch(&self, timeout: Option<pps_ktime>) -> Result<pps_fdata, Error> {
+    pub fn fetch(&self, timeout: Option<pps_ktime>) -> Result<pps_fdata> {
         let timeout = timeout.unwrap_or(pps_ktime {
             sec: 0,
             nsec: 0,
